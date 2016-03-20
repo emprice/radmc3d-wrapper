c90815f58f10cb107cf8c394150c64247745763d
Ellen Price <ellen.price@cfa.harvard.edu>
2016-03-20 11:00:55 -0400

    Fixing cgs module to use class properties

--- cgs.py
+++ cgs.py
@@ -1,43 +1,91 @@
 # -*- coding: utf-8 -*-
 
-class cgs(object):
+class ClassPropertyDescriptor(object):
+    '''
+    See `this StackOverflow post <http://stackoverflow.com/a/5191224/1552418>`_.
+    This snippet is the intellectual property of its original author.
+    '''
 
-    @property
+    def __init__(self, fget, fset=None):
+        self.fget = fget
+        self.fset = fset
+
+    def __get__(self, obj, klass=None):
+        if klass is None:
+            klass = type(obj)
+        return self.fget.__get__(obj, klass)()
+
+    def __set__(self, obj, value):
+        if not self.fset:
+            raise AttributeError('can\'t set attribute')
+        type_ = type(obj)
+        return self.fset.__get__(obj, type_)(value)
+
+    def setter(self, func):
+        if not isinstance(func, (classmethod, staticmethod)):
+            func = classmethod(func)
+        self.fset = func
+        return self
+
+
+
+def classproperty(func):
+    '''
+    See `this StackOverflow post <http://stackoverflow.com/a/5191224/1552418>`_.
+    This snippet is the intellectual property of its original author.
+    '''
+
+    if not isinstance(func, (classmethod, staticmethod)):
+        func = classmethod(func)
+
+    return ClassPropertyDescriptor(func)
+
+
+
+class cgs(object):
+    '''
+    Accessors for values of common astronomical quantities in CGS
+    (centimeters-grams-seconds) units.
+    '''
+
+    @classproperty
     def au(self):
         '''Astronomical unit, cm'''
         return 1.49597870700e13
 
-    @property
+    @classproperty
     def c(self):
         '''Speed of light in vacuum, cm/s'''
         return 2.99792458e10
 
-    @property
+    @classproperty
     def G(self):
-        '''Newtonian gravitational constant, cm^3/g/s^2'''
+        '''Newtonian gravitational constant, cm:math:`{}^3`/g/s:math:`{}^2`'''
         return 6.67384e-8
 
-    @property
+    @classproperty
     def LSun(self):
         '''Solar luminosity, erg/s'''
         return 3.828e33
 
-    @property
+    @classproperty
     def MEarth(self):
         '''Earth mass, g'''
         return 5.9726e27
 
-    @property
+    @classproperty
     def MSun(self):
         '''Solar mass, g'''
         return 1.9885e33
 
-    @property
+    @classproperty
     def REarth(self):
         '''Earth radius, cm'''
         return 6.378137e8
 
-    @property
+    @classproperty
     def RSun(self):
         '''Solar radius, cm'''
         return 6.9551e10
+
+# vim: set ft=python:
--- docs/api.rst
+++ docs/api.rst
@@ -4,25 +4,20 @@ Full API documentation
 cgs module
 ----------
 
-.. automodule:: cgs
+.. autoclass:: cgs.cgs
     :members:
-    :undoc-members:
-    :show-inheritance:
 
 configuration module
 --------------------
 
 .. automodule:: configuration
     :members:
-    :undoc-members:
-    :show-inheritance:
 
 coordsys module
 ---------------
 
 .. automodule:: coordsys
     :members:
-    :undoc-members:
     :show-inheritance:
 
 dust module
@@ -30,47 +25,36 @@ dust module
 
 .. automodule:: dust
     :members:
-    :undoc-members:
-    :show-inheritance:
 
 fileio module
 -------------
 
 .. automodule:: fileio
     :members:
-    :undoc-members:
-    :show-inheritance:
 
 grid module
 -----------
 
 .. automodule:: grid
     :members:
-    :undoc-members:
-    :show-inheritance:
 
 render module
 -------------
 
 .. automodule:: render
     :members:
-    :undoc-members:
-    :show-inheritance:
 
 simulation module
 -----------------
 
 .. automodule:: simulation
     :members:
-    :undoc-members:
-    :show-inheritance:
 
 star module
 -----------
 
 .. automodule:: star
     :members:
-    :undoc-members:
     :show-inheritance:
 
 vectorsys module
@@ -78,5 +62,4 @@ vectorsys module
 
 .. automodule:: vectorsys
     :members:
-    :undoc-members:
     :show-inheritance:
